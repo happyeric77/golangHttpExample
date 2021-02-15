d01a3506a3121b1b61c2acfd7e4cf896ae2eb898
Eric <happyeric76@hotmail.com>
2021-02-15 14:22:20 +0900

    HandleFunc

--- main.go
+++ main.go
@@ -6,27 +6,20 @@ import (
 )
 
 
-// Use http.ServeMux to implement multiplexer (mux) routing.
+// Use http.HandleFunc to implement multiplexer (mux) routing.
 
-type case1 int
-
-type case2 int
-
-func (c case1) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+func case1(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(res, "This is case1")
 }
 
-func (c case2) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+func case2(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(res, "This is case2")
 }
 
-
 func main() {
-	mux := http.NewServeMux()
-	var c1 case1
-	var c2 case2
-	mux.Handle("/case1", c1)
-	mux.Handle("/case2", c2)
+
+	http.HandleFunc("/case1", case1)
+	http.HandleFunc("/case2", case2)
 	
-	http.ListenAndServe(":1234", mux)
+	http.ListenAndServe(":1234", nil)
 }
