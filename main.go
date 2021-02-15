1d05c992fa9dfee2c0c944eee451e6355103a6a2
Eric <happyeric76@hotmail.com>
2021-02-15 13:35:49 +0900

    Mux feature

--- main.go
+++ main.go
@@ -5,10 +5,23 @@ import (
 	"net/http"	
 )
 
+
+// Use http.Handler to check URL path to implement multiplexer (mux)
+type foo int 
+func(f foo) ServeHTTP (res http.ResponseWriter, req *http.Request){
+	path := req.URL.Path
+	switch path {
+	case "/case1":
+		fmt.Fprint(res, "This is case1")
+	case "/case2":
+		fmt.Fprint(res, "This is case2")
+	default:
+		fmt.Fprintf(res, "Server does not support this endpoint(%s).\nServer only supports '/case1' & '/case2'", path)
+	}
+}
+
 func main() {
-	// Pass the end point and handler function
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request){
-		fmt.Println("Default http routing function")
-	})
-	http.ListenAndServe(":1234", nil)
+	http.ServeMux
+	var f foo
+	http.ListenAndServe(":1234", f)
 }
